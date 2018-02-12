106408aaebd742c39e85ab44aa5ce3e720f25362
doriguzman <dorianguzman@ac.c4q.nyc>
2018-02-12 16:29:44 -0500

    rendering prof images

---
+++ frontend/src/components/LoggedInUser/profileImages.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+const styles = {
+    img: {
+        height: "10em"
+    }
+
+};
+
+
+class Images extends Component {
+    constructor() {
+        super();
+        this.state = {
+            images: []
+        }
+    }
+
+
+    componentDidMount = () => {
+        axios
+            .get('LINK HEREEEEEE')
+            .then (res =>{
+                console.log(res.data)
+                this.setState({
+                    images: res.data.data.images
+                })
+            })
+                .catch(err => {
+                    console.log(err)
+                })
+        }
+   
+    render() {
+        const { images } = this.state
+
+        return (
+            <div>
+                <div class='rowOfPics'> 
+
+                </div>
+            
+                <ul>
+                    {dogs.map(dog => (
+                        <li>
+                            <img  alt='img' src={img} />
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Images
