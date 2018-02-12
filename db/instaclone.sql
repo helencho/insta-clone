319fc8d2a6cd1c123100bfd4f738f1ca21355838
princessguerrero <princessguerrero@ac.c4q.nyc>
2018-02-12 15:06:19 -0500

    changed ids from varchar to integer

--- db/instaclone.sql
+++ db/instaclone.sql
@@ -19,35 +19,35 @@ INSERT INTO users (username, password_digest, email_add, fullname, profile_pic, 
 
 CREATE TABLE photos (
     photo_id SERIAL PRIMARY KEY,
-    user_id VARCHAR,
+    user_id INTEGER,
     photo_link VARCHAR,
     caption VARCHAR
 );
 
 INSERT INTO photos (user_id, photo_link, caption)
-    VALUES ('1', 'https://i.imgur.com/GAbRAat.jpg', 'exactly');
+    VALUES (1, 'https://i.imgur.com/GAbRAat.jpg', 'exactly');
 
 CREATE TABLE likes (
-    user_id VARCHAR,
-    photo_id VARCHAR
+    user_id INTEGER,
+    photo_id INTEGER
 );
 
 INSERT INTO likes (user_id, photo_id)
-    VALUES ('1', '1');
+    VALUES (1, 1);
 
 
 CREATE TABLE user_followers (
-    user_id VARCHAR,
-    follower_id VARCHAR
+    user_id INTEGER,
+    follower_id INTEGER
 );
 
 INSERT INTO user_followers (user_id, follower_id)
-    VALUES ('1', '3');
+    VALUES (1, 3);
 
 CREATE TABLE user_following (
-    user_id VARCHAR,
-    following_id VARCHAR
+    user_id INTEGER,
+    following_id INTEGER
 );
 
 INSERT INTO user_following (user_id, following_id)
-    VALUES ('1', '2');
+    VALUES (1, 2);
