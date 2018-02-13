c189337e1a96412c2fd57a86601ef10963909be1
helencho <helencho@ac.c4q.nyc>
2018-02-13 10:25:03 -0500

    working on ajax requests for grabbing photo and photo details

--- frontend/src/components/LoggedInUser/SinglePhoto.jsx
+++ frontend/src/components/LoggedInUser/SinglePhoto.jsx
@@ -1,10 +1,42 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class SinglePhoto extends Component {
+    constructor() {
+        super()
+        this.state = {
+            photoUrl: ''
+        }
+    }
+
+    componentDidMount() {
+        this.getSinglePhoto() 
+    }
+
+    getSinglePhoto = () => {
+        const id = props.params.match.id
+
+        axios
+            .get(`/p/${id}`)
+            .then(res => {
+                console.log(res.data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    getPhotoDetails = () => {
+        // /p/:id/details
+    }
+
     render() {
+        const { photoUrl } = this.state
+
         return (
-            <div>
-                </div>
+            <div className='single-photo-container'>
+                {/* <img src={photoUrl} alt='photo' /> */}
+            </div>
         )
     }
 }
