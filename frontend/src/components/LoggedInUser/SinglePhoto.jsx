1d8024ff8c04530639469b4a2d29e0c209178369
helencho <helencho@ac.c4q.nyc>
2018-02-13 10:31:51 -0500

    working on grabbing users who like the photo, and rendering an empty/full heart depending on if current user likes the photo

--- frontend/src/components/LoggedInUser/SinglePhoto.jsx
+++ frontend/src/components/LoggedInUser/SinglePhoto.jsx
@@ -5,7 +5,10 @@ class SinglePhoto extends Component {
     constructor() {
         super()
         this.state = {
-            photoUrl: ''
+            photoUrl: '',
+            photoCaption: '',
+            likedByUsers: [],
+            liked: false
         }
     }
 
@@ -32,6 +35,7 @@ class SinglePhoto extends Component {
 
     render() {
         const { photoUrl } = this.state
+        console.log(this.state) 
 
         return (
             <div className='single-photo-container'>
