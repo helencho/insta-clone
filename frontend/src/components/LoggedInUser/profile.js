2bb9b6c585319a927c24121ef96eb87034d4283f
doriguzman <dorianguzman@ac.c4q.nyc>
2018-02-13 11:07:02 -0500

    finished photos

--- frontend/src/components/LoggedInUser/profile.js
+++ frontend/src/components/LoggedInUser/profile.js
@@ -1,28 +1,52 @@
 import React from 'react'
 import { Route, Link, Switch } from "react-router-dom"
 import '../App.css'
+import axios from "axios";
 
 
 class Profile extends React.Component {
     constructor() {
         super();
         this.state = {
-            user: ''
+            images: []
         }
     }
 
 
+    componentDidMount = () => {
+        axios
+            .get('/users/p')
+            .then (res =>{
+                console.log(res.data.data)
+                const photoData= res.data.data
+                console.log(photoData.map(photo=> photo.photo_link))
+                this.setState({
+                    images: res.data.data
+                })
+            })
+                .catch(err => {
+                    console.log(err)
+                })
+        }
+   
+    render() {
+        const { images } = this.state
 
-render(){
-return (
-    <div id='header'>
-        <div className='topbar'>
-          <h1> Instagram</h1>
-        </div>
-        </div>
+        return (
+            <div>
+                <div class='rowOfPics'> 
 
-)
-}
+                </div>
+                
+                    {images.map(img => (
+                        <figure id={img.photo_id}>
+                            <img  alt='img' src={img.photo_link} width='223' height='223'/>
+                            {/* <figcaption> {img.caption} </figcaption> */}
+                        </figure>
+                    ))}
+            </div>
+        )
+    }
 }
 
 export default Profile 
