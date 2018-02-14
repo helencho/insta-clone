25913724486fcce61cf5115e58135c119e472a3b
helencho <helencho@ac.c4q.nyc>
2018-02-14 10:46:40 -0500

    Added more images to database

--- backend/db/instaclone.sql
+++ backend/db/instaclone.sql
@@ -22,27 +22,35 @@ INSERT INTO users (username, password_digest, email_add, fullname, profile_pic, 
 
 CREATE TABLE photos (
     photo_id SERIAL PRIMARY KEY,
-    user_id INTEGER,
+    user_id INTEGER REFERENCES users,
     photo_link VARCHAR,
     caption VARCHAR
 );
 
 INSERT INTO photos (user_id, photo_link, caption)
     VALUES (1, 'https://i.imgur.com/GAbRAat.jpg', 'exactly'),
-    (1, 'https://i.imgur.com/wuRt0VA.jpg', 'pupz')
+    (1, 'https://i.imgur.com/U5E9wVu.jpg', '<3'),
+    (2, 'https://i.imgur.com/ZdQOwq1.jpg', 'sea and sky'),
+    (2, 'https://i.imgur.com/Iuoi7Gg.jpg', 'paradise'),
+    (2, 'https://i.imgur.com/vqGctDg.jpg', 'some profound quote here'),
+    (2, 'https://i.imgur.com/RdjNiph.jpg', 'Me in my natural state'),
+    (3, 'https://i.imgur.com/3eAA6j5.jpg', 'My best friend'),
+    (3, 'https://i.imgur.com/EZ0Qnxg.jpg', 'Playing tourist.'),
+    (3, 'https://i.imgur.com/BumKSM3.jpg', 'Random doggo');
 
 CREATE TABLE likes (
-    user_id INTEGER,
-    photo_id INTEGER
+    user_id INTEGER REFERENCES users,
+    photo_id INTEGER REFERENCES photos
 );
 
 INSERT INTO likes (user_id, photo_id)
     VALUES (1, 1),
-    (2, 1);
+    (2, 1),
+    (3, 1);
 
 
 CREATE TABLE user_followers (
-    user_id INTEGER,
+    user_id INTEGER REFERENCES users,
     follower_id INTEGER
 );
 
@@ -50,7 +58,7 @@ INSERT INTO user_followers (user_id, follower_id)
     VALUES (1, 3);
 
 CREATE TABLE user_following (
-    user_id INTEGER,
+    user_id INTEGER REFERENCES users,
     following_id INTEGER
 );
 
