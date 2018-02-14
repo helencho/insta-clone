58d484cbb5785dc88e03d52861495edbe37ad5e9
helencho <helencho@ac.c4q.nyc>
2018-02-14 14:01:44 -0500

    Created stylesheet for home component

--- frontend/src/components/LoggedInUser/Home.jsx
+++ frontend/src/components/LoggedInUser/Home.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
+import '../Home.css'
 
 class Home extends Component {
     constructor(props) {
