335388569dae570589b99d4659ac546c3c25df46
doriguzman <dorianguzman@ac.c4q.nyc>
2018-02-15 13:19:14 -0500

    editing followers/following

--- frontend/src/components/LoggedInUser/Followers.jsx
+++ frontend/src/components/LoggedInUser/Followers.jsx
@@ -2,6 +2,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import "../Followers.css"
+import Profile from './profile'
 
 class Followers extends Component {
     constructor(props) {
@@ -9,6 +10,12 @@ class Followers extends Component {
         this.state = {
             allFollowing: this.props.followers
         }
+    }
+
+
+    onClick = (user) =>{
+        return <Profile user={user} />
+
     }
 
     render() {
@@ -22,15 +29,15 @@ class Followers extends Component {
                 <div>
                     {this.props.followers.map(user => (
                         <div className='users' id={user.follower_id}>
-                            <div>
-                                <Link to={`/users/u/${user.follower_id}/profile`} >
+                            <div onClick={this.onClick(user)} >
+                                {/* <Link to={`/users/u/${user.follower_id}/profile`} > */}
                                     <div class='FollowerProfilePic'>
                                         <img src={user.profile_pic} />
                                     </div>
                                     <div className='FollowerUsername'>
                                         <h2>{user.username} </h2>
                                     </div>
-                                </Link>
+                                {/* </Link> */}
 
                                 <div className='FollowerFullName'>
                                     <h2> {user.fullname} </h2>
